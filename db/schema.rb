d53fa7594f314ac7e69a7a1074e459f3e9759c43
hellijah <thierry.corlieto@gmail.com>
2024-11-27 13:26:43 +0100

    BDD update with joint table orderitems // add orderscontroller with views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_25_162328) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_27_105720) do
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id", null: false
     t.integer "item_id", null: false
@@ -19,10 +19,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_25_162328) do
     t.index ["cart_id"], name: "index_cart_items_on_cart_id"
     t.index ["item_id"], name: "index_cart_items_on_item_id"
   end
-
-    # il faudra rajouter une colonne quantity dans le panier et dans la commande
-    # il faudra rajouter une table de liaison OrderItemS
-
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -35,18 +31,28 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_25_162328) do
     t.string "name"
     t.text "description"
     t.decimal "price"
-    t.integer "stock" #, default: 1, null: false # pour quand on gèrera les quantités
+    t.integer "stock"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "item_id", null: false
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.string "status"
     t.decimal "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -72,6 +78,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_25_162328) do
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "items"
   add_foreign_key "carts", "users"
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
   add_foreign_key "sessions", "users"
 end
