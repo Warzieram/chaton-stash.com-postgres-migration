556c1186e7e66e366fde42e6c16ea599e40cbcf1
hellijah <thierry.corlieto@gmail.com>
2024-11-27 13:26:43 +0100

    BDD update with joint table orderitems // add orderscontroller with views

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,16 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :items
+  has_many :items, through: :order_items
+  has_many :order_items
+
+  enum status: { pending: 'pending', paid: 'paid', cancelled: 'cancelled', shipped: 'shipped', delivered: 'delivered' }
+
+  # Optionnel : valeur par défaut lors de la création
+  after_initialize :set_default_status, if: :new_record?
+
+  private
+
+  def set_default_status
+    self.status ||= :pending
+  end
 end
